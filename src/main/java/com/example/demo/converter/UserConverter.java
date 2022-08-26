cd3c50fe0ffca87814e69739b52fc7dc54894418
James <nguyenvanviet1210@gmail.com>
2022-08-26 03:39:52 -0700

    third commit

--- src/main/java/com/example/demo/converter/UserConverter.java
+++ src/main/java/com/example/demo/converter/UserConverter.java
@@ -9,6 +9,7 @@ public class UserConverter {
 
     public UserDto toDto(User user) {
         UserDto userDto = new UserDto();
+        
         userDto.setId(user.getId());
         userDto.setFirstName(user.getFirstName());
         userDto.setLastName(user.getLastName());
